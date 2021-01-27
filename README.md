b95c5eee80fa1d1360ec6a13d1d242d1d0bafeb6
Jacob Berg <coffeesnakes@gmail.com>
2021-01-26 21:35:06 -0800

    proxy setup

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # affordability-proxy-server
+
+
+
+INDEX.HTML
+
+<!-- <!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="UTF-8">
+    <title>Archon Design</title>
+    <link rel='stylesheet' href='style.css'>
+<script src="https://kit.fontawesome.com/bbcab79eff.js" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <img id="nav" src="static/truliaNav.png" />
+    <div>THIS WAS NOT A TRIUMPH</div>
+     <div id="PhotoGallery"></div>
+     <div id="similar"></div>
+     <div id="local-review"></div>
+     <div id="affordability"></div>
+     <script src="bundles/affordability"></script>
+     <script src="bundles/similar"></script>
+    <img id='footer' src='static/footer.png' />
+    <!-- <script src="http://localhost:3002/bundle.js"></script>
+    <script src="http://localhost:3001/bundle.js"></script>
+    <script src="http://localhost:3004/bundle.js"></script>
+    <script src="http://localhost:3003/bundle.js"></script> -->
+  </body>
+
+
+</html> -->
--- server/index.js
+++ server/index.js
@@ -1,11 +1,58 @@
-const path = require('path');
-const express = require('express');
-const port = 3005;
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const port = 3000;
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..public')));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log(`listening on port:${port}..`))
+//Gallery
+app.use(
+  "/api/homeinfo",
+  createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
+app.use(
+  "/api/homeimages",
+  createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true })
+);
+
+//Similar Carousel
+app.use(
+  "/api/homes/similar",
+  createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true })
+);
+app.use(
+  "/api/homes/nearby",
+  createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true })
+);
+
+//Local Review
+app.use(
+  "/reviews",
+  createProxyMiddleware({ target: "http://localhost:3004", changeOrigin: true })
+);
+app.use(
+  "/features",
+  createProxyMiddleware({ target: "http://localhost:3004", changeOrigin: true })
+);
+
+//Calculator
+app.use(
+  "/mortgages",
+  createProxyMiddleware({ target: "http://localhost:3003", changeOrigin: true })
+);
+app.use(
+  "/homes",
+  createProxyMiddleware({ target: "http://localhost:3003", changeOrigin: true })
+);
+
+
+app.listen(port, () => {
+  console.log(`proxy on port: ${port}`);
+});
